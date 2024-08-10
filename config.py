964834666ae47d3e9e14516b36ed2cfeebd47b54
Vipgod2 <instaclonerboy@gmail.com>
2024-08-10 10:59:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7391520101:AAF0pA1SByHcJV2tDEMSQtA4RMJOkDyOBSA")
+      API_ID = int(os.environ.get("APP_ID", "23416992"))
+      API_HASH = os.environ.get("API_HASH", "bebe7b8b7bc74aeaa08f658e8aa799fe")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
-      DB_URL = os.environ.get("DATABASE_URL", "")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "TG_SUPP0RT")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", "7030346964" )) 
+      DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botss:botss@cluster0.txf7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
