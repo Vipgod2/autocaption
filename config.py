0d868feb56f7e207a2e26c635ce9b51b2676d487
Vipgod2 <instaclonerboy@gmail.com>
2024-08-10 11:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7391520101:AAF0pA1SByHcJV2tDEMSQtA4RMJOkDyOBSA")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7391520101:AAF0pA1SByHcJV2tDEMSQtA4RMJOkDyOBS")
       API_ID = int(os.environ.get("APP_ID", "23416992"))
       API_HASH = os.environ.get("API_HASH", "bebe7b8b7bc74aeaa08f658e8aa799fe")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
